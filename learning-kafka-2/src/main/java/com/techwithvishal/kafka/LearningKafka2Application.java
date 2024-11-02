7fb95a7bec2d5008ffd65c5286ffb638c2b463ff
Vishall0317 <vishalbramhankar18@gmail.com>
2024-11-02 11:47:23 +0530

    initial commit

---
+++ learning-kafka-2/src/main/java/com/techwithvishal/kafka/LearningKafka2Application.java
@@ -0,0 +1,13 @@
+package com.techwithvishal.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningKafka2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningKafka2Application.class, args);
+	}
+
+}
